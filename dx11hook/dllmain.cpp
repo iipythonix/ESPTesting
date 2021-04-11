d102fe81b9c6db102a0bac9548eb2072432059cf
iipythonix <79859739+iipythonix@users.noreply.github.com>
2021-04-11 17:38:22 -0400

    Add files via upload

---
+++ dx11hook/dllmain.cpp
@@ -0,0 +1,13 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+#include <iostream>
+
+HINSTANCE DllHandle;
+
+#include <D3D11.h>
+#include <D3DX11.h>
+
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "D3DX11.lib")
+
